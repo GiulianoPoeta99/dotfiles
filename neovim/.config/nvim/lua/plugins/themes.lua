9c850f5a8ecc7f3e0d323bd72c05932b551f39a0
Giuliano Ignacio Poeta <giuliano.poeta.laboral@gmail.com>
2024-06-17 12:27:27 -0300

    se cambio nombre de dir

---
+++ neovim/.config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,60 @@
+-- setup theme
+return {
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "catppuccin",
+				},
+			})
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
+	{
+		"uloco/bluloco.nvim",
+		lazy = false,
+		priority = 1000,
+		dependencies = { "rktjmp/lush.nvim" },
+	},
+	{
+		"zaldih/themery.nvim",
+		config = function()
+			require("themery").setup({
+				themes = {
+					{
+						name = "Catppuccin latte",
+						colorscheme = "catppuccin-latte",
+					},
+					{
+						name = "Catppuccin frappe",
+						colorscheme = "catppuccin-frappe",
+					},
+					{
+						name = "Catppuccin macchiato",
+						colorscheme = "catppuccin-macchiato",
+					},
+					{
+						name = "Catppuccin mocha",
+						colorscheme = "catppuccin-mocha",
+					},
+					{
+						name = "Bluloco dark",
+						colorscheme = "bluloco-dark",
+					},
+					{
+						name = "Bluloco light",
+						colorscheme = "bluloco-light",
+					},
+				},
+			})
+		end,
+	},
+}
