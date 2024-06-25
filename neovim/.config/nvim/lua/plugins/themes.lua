2edafa1a00382252b29134fa62fac44c63c2a329
Giuliano Ignacio Poeta <giuliano.poeta.laboral@gmail.com>
2024-06-25 10:05:10 -0300

    se agrego plugin a nvim

--- neovim/.config/nvim/lua/plugins/themes.lua
+++ neovim/.config/nvim/lua/plugins/themes.lua
@@ -1,69 +1,66 @@
 -- setup theme
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("catppuccin-mocha")
-		end,
-	},
-	{
-		"uloco/bluloco.nvim",
-		lazy = false,
-		priority = 1000,
-		dependencies = { "rktjmp/lush.nvim" },
-		-- config = function()
-		-- 	vim.cmd.colorscheme("bluloco-dark")
-		-- end,
-	},
-	{
-		"Mofiqul/dracula.nvim",
-		lazy = false,
-		priority = 1000,
-		-- config = function()
-		-- 	vim.cmd.colorscheme("dracula")
-		-- end,
-	},
-	{
-		"zaldih/themery.nvim",
-		config = function()
-			require("themery").setup({
-				themes = {
-					{
-						name = "Catppuccin latte",
-						colorscheme = "catppuccin-latte",
-					},
-					{
-						name = "Catppuccin frappe",
-						colorscheme = "catppuccin-frappe",
-					},
-					{
-						name = "Catppuccin macchiato",
-						colorscheme = "catppuccin-macchiato",
-					},
-					{
-						name = "Catppuccin mocha",
-						colorscheme = "catppuccin-mocha",
-					},
-					{
-						name = "Bluloco dark",
-						colorscheme = "bluloco-dark",
-					},
-					{
-						name = "Bluloco light",
-						colorscheme = "bluloco-light",
-					},
-					{
-						name = "Dracula",
-						colorscheme = "dracula",
-					},
-					{
-						name = "Dracula Soft",
-						colorscheme = "dracula-soft",
-					},
-				},
-			})
-		end,
-	},
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
+  {
+    "uloco/bluloco.nvim",
+    lazy = false,
+    priority = 1000,
+    dependencies = { "rktjmp/lush.nvim" },
+  },
+  {
+    "Mofiqul/dracula.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+  {
+    "zaldih/themery.nvim",
+    config = function()
+      require("themery").setup({
+        themes = {
+          {
+            name = "Catppuccin latte",
+            colorscheme = "catppuccin-latte",
+          },
+          {
+            name = "Catppuccin frappe",
+            colorscheme = "catppuccin-frappe",
+          },
+          {
+            name = "Catppuccin macchiato",
+            colorscheme = "catppuccin-macchiato",
+          },
+          {
+            name = "Catppuccin mocha",
+            colorscheme = "catppuccin-mocha",
+          },
+          {
+            name = "Bluloco dark",
+            colorscheme = "bluloco-dark",
+          },
+          {
+            name = "Bluloco light",
+            colorscheme = "bluloco-light",
+          },
+          {
+            name = "Dracula",
+            colorscheme = "dracula",
+          },
+          {
+            name = "Dracula Soft",
+            colorscheme = "dracula-soft",
+          },
+        },
+      })
+    end,
+  },
 }
