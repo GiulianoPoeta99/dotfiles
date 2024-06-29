e29ae94cf8a2aabf48e5aedf9e83cc50aba69e14
Giuliano Ignacio Poeta <giuliano.poeta.laboral@gmail.com>
2024-06-29 19:49:37 -0300

    se elimino qtile

--- qtile/.config/qtile/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Verificar si el monitor Samsung está conectado
-if hwinfo --monitor | grep -q "SAMSUNG LF24T35"; then
-	echo "Monitor Samsung detectado. Ejecutando comando xrandr..."
-	# Ejecutar el comando xrandr
-	xrandr --output HDMI-1-0 --mode 1920x1080 --above eDP-1 && xrandr --output HDMI-1-0 --rotate inverted
-	echo "Comando xrandr ejecutado."
-fi
