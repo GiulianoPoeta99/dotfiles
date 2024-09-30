3800a98fb7ae3b4ef47723f6f33525f0f522cf24
Giuliano Poeta <giuliano.poeta.laboral@gmail.com>
2024-09-29 22:06:02 -0300

    Ss agregaron paquetes

--- install.sh
+++ install.sh
@@ -191,7 +191,7 @@ EOF
     )
 
     # Install additional packages
-    sudo pacman -S --noconfirm neovim ripgrep lazygit fd xclip bash-completion eza bat glow man-db man-pages python-pip flatpak podman go php composer jdk22-openjdk lua luarocks obsidian discord zed
+    sudo pacman -S --noconfirm neovim ripgrep lazygit fd xclip bash-completion eza bat glow man-db man-pages python-pip flatpak podman go php composer jdk22-openjdk lua luarocks obsidian discord zed fastfetch
     paru -S --noconfirm lazydocker brave-bin slack-desktop zen-browser-bin
 
     configure_docker
