91fc674e9e52c2fb6fcca5174438a0169341e085
Giuliano Ignacio Poeta <giuliano.poeta.laboral@gmail.com>
2025-01-17 16:12:22 -0300

    se mejoro script

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -63,8 +63,8 @@ install_all() {
 
   # Node.js
   # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  # export NVM_DIR="$HOME/.nvm"
+  # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
   nvm install 22
 
   # Docker
