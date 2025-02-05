630c3b0c1f32d5f11e5a5615c83185334a3ee5de
Giuliano Ignacio Poeta <giuliano.poeta.laboral@gmail.com>
2025-02-05 13:51:23 -0300

    hay un error cuando se quiere centrar le widget al centro

--- .config/ags/widget/NotificationPopups.tsx
+++ .config/ags/widget/NotificationPopups.tsx
@@ -90,14 +90,14 @@ class NotifiationMap implements Subscribable {
 }
 
 export default function NotificationPopups(gdkmonitor: Gdk.Monitor) {
-    const { TOP, CENTER } = Astal.WindowAnchor
+    const { TOP, RIGHT } = Astal.WindowAnchor
     const notifs = new NotifiationMap()
 
     return <window
         className="NotificationPopups"
         gdkmonitor={gdkmonitor}
         exclusivity={Astal.Exclusivity.EXCLUSIVE}
-        anchor={TOP | CENTER}>
+        anchor={TOP | RIGHT}>
         <box vertical>
             {bind(notifs)}
         </box>
