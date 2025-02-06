1dcc9358af6e193c445e497c27da90c094567684
Giuliano Ignacio Poeta <giuliano.poeta.laboral@gmail.com>
2025-02-06 18:00:37 -0300

    se mejoro config de hypr

--- .config/hypr/scripts/datetime.sh
+++ .config/hypr/scripts/datetime.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 fecha=$(date +"Fecha: %A | %d.%m.%Y")
-hora=$(date +"Hora: %H:%M")
+hora=$(date +"Hora: %H:%M:%S")
 
 notify-send -i "dialog-information" -t 5000 -u low " " "\n$hora\n\n$fecha"
