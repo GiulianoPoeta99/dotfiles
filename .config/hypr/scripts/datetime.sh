771b3e7e6a20ffb7de5f028170e0b05e9c4139cf
Giuliano Ignacio Poeta <giuliano.poeta.laboral@gmail.com>
2025-02-10 21:59:28 -0300

    se mejoro notif de hora y ahora es status

--- .config/hypr/scripts/datetime.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-fecha=$(date +"%A %H:%M:%S | %d/%m/%Y")
-
-notify-send -i "dialog-information" -t 5000 -u low "$fecha"
---
+++ .config/hypr/scripts/status.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+hora=$(date +"%A %H:%M:%S | %d/%m/%Y")
+fecha=$(date +"%d/%m/%Y")
+bateria=$(upower -i $(upower -e | grep 'BAT') | grep -E "percentage" | awk '{print $2}')
+
+notify-send -i "dialog-information" -t 5000 -u low "Status" "* Time: $hora\n* Date: $fecha\n* Battery: $bateria"
