505eedcb8d9958279ab8a3036240b482ee6b44c5
Giuliano Ignacio Poeta <giuliano.poeta.laboral@gmail.com>
2025-03-14 14:21:46 -0300

    se mejoro alias de pulseaudio

--- .config/zsh/alias/ags/pulseaudio.sh
+++ .config/zsh/alias/ags/pulseaudio.sh
@@ -1,2 +1,2 @@
-alias pal="pactl list short sinks"
+alias pal="nu -c 'pactl list short sinks | detect columns --no-headers | select column0 column1 column2 column3 column4 column5 column6 | rename ID NAME DRIVER FORMAT CHANNEL FRECUENCY STATE'"
 alias pas="pactl set-default-sink"
